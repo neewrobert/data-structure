ad265f3fb71a7eda3f4d945c71ac1404227fb9ea
Newton dos Santos <neewrobert@gmail.com>
2023-02-26 20:21:47 +0100

    add failed test scenario

--- src/test/java/com/neewrobert/datastructure/search/binary/BinarySearchTest.java
+++ src/test/java/com/neewrobert/datastructure/search/binary/BinarySearchTest.java
@@ -11,24 +11,30 @@ import java.util.stream.IntStream;
 
 public class BinarySearchTest {
 
-
     @Test
     public void binarySearchSuccessTest() {
-
         int size = 25_000;
         Integer target = new Random().nextInt(size - 1);
 
-        Assertions.assertEquals(BinarySearch.binarySearch(generateSortedListOfIntegers(size), target), target);
-
+        List<Integer> searchable = generateListOfIntegers(size).stream().sorted().toList();
+        Assertions.assertEquals(BinarySearch.binarySearch(searchable, searchable.get(target)), target);
     }
 
+    @Test
+    public void binarySearchFailedForUnorderedListTest() {
 
-    private List<Integer> generateSortedListOfIntegers(int max) {
+        int size = 25_000;
+        int target = new Random().nextInt(size - 1);
+
+        List<Integer> searchable = generateListOfIntegers(size);
+        Assertions.assertEquals(BinarySearch.binarySearch(searchable, searchable.get(target)), -1);
+    }
+
+    private List<Integer> generateListOfIntegers(int max) {
         List<Integer> list = new CopyOnWriteArrayList<>();
         IntStream.range(0, max)
                 .parallel()
                 .forEach(list::add);
-        return list.stream().sorted().toList();
-
+        return list.stream().toList();
     }
 }
