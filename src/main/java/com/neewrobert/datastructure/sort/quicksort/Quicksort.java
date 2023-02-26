7b7c1a7d55ff79c4ab2d4cc31569eeb9d9b2525e
Newton dos Santos <neewrobert@gmail.com>
2023-02-26 21:54:23 +0100

    create structure for Quicksort.java

---
+++ src/main/java/com/neewrobert/datastructure/sort/quicksort/Quicksort.java
@@ -0,0 +1,13 @@
+package com.neewrobert.datastructure.sort.quicksort;
+
+import java.util.List;
+
+public class Quicksort {
+
+    public static <T> List<T> sort(List<? extends Comparable<? super T>> toOrder){
+
+
+
+        return null;
+    }
+}
